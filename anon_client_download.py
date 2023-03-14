b4a805555a9006c28c33f88de627ddfe9c9d8284
MaanasVohra <maanasvohra1998@gmail.com>
2023-03-13 21:16:19 -0700

    added multiple hosts retrieve file functionality

--- anon_client_download.py
+++ anon_client_download.py
@@ -3,7 +3,7 @@ import util
 
 # Establishing FTP Connection using anonymous client
 ftp = ftplib.FTP()
-ftp.connect(host=util.SERVER_ADDRESS, port=util.SERVER_PORT)
+ftp.connect(host=util.SERVER_ADDRESS, port=util.SERVER_PORT_MP["1"]) # connect to server 1
 ftp.login("anonymous", "anystring")
 
 # # List directory contents
@@ -11,10 +11,9 @@ ftp.login("anonymous", "anystring")
 
 # Download a file
 remote_file = "server_file.txt"
-filename = "downloaded.txt"
+filename = "downloaded_anon_client.txt"
 localfile = open(filename, "wb")
 ftp.retrbinary("RETR " + remote_file, localfile.write, 1024)
 localfile.close()
-
 # Closing FTP Connection
 ftp.quit()
--- server.py
+++ server.py
@@ -5,7 +5,41 @@ import util
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
+import ftplib
 
+import threading
+
+# remote_server_number = input("Enter remote server number: ") # 2
+REMOTE_SERVER_PORT = util.SERVER_PORT_MP["2"]
+
+class AnonConnFTPHandler(FTPHandler):
+
+    def on_connect(self):
+        self.remote_ftp = ftplib.FTP()
+        self.remote_ftp.connect(util.SERVER_ADDRESS, REMOTE_SERVER_PORT)
+        self.remote_ftp.login("anonymous", "anystring") # anonymous connection
+
+    def ftp_RETR(self, file):
+        print("RETR requested for: ", file)
+        remote_file = file.split("/")[-1] # Unix-based
+        filename = util.SERVER_DIRECTORY + "/" + "server_file_50002.txt"
+        localfile = open(filename, "wb")
+        self.remote_ftp.retrbinary("RETR " + remote_file, localfile.write, 1024)
+        localfile.close()
+        self.remote_ftp.quit()
+        super().ftp_RETR("server/server_file_50002.txt")
+
+# Local server config
+local_authorizer = DummyAuthorizer()
+local_authorizer.add_anonymous(util.SERVER_DIRECTORY, perm=('r', 'w'))
+local_handler = AnonConnFTPHandler # 2
+local_handler.authorizer = local_authorizer
+
+# create FTP server and serve
+server = FTPServer((util.SERVER_ADDRESS, util.SERVER_PORT_MP["1"]), local_handler) # 1 -> Change later
+server.serve_forever()
+
+"""
 # Define FTP Server Configuration
 FTP_HOST = util.SERVER_ADDRESS
 FTP_PORT = util.SERVER_PORT
@@ -26,3 +60,4 @@ server = FTPServer((FTP_HOST, FTP_PORT), handler)
 
 # Start FTP Server
 server.serve_forever()
+"""
