bb47939d2cbac2982faec6f67d931ce08344ba4f
MaanasVohra <maanasvohra1998@gmail.com>
2023-03-16 23:06:29 -0700

    upload: multiple servers + command line

--- README.md
+++ README.md
@@ -5,6 +5,10 @@
 2. Server2: python server.py ./server_50002 50002 50003
 3. Server3: python server.py ./server_50003 50003
 
-## Usage - Client:
+## Usage - Client Download:
 1. Download client for no switching: python anon_client_download.py 50000 random1 
 2. Download client for switching: python anon_client_download.py 50000 server_50003_file.txt:50002:50003
+
+## Usage - Client Upload:
+1. Upload client for no switching: python anon_client_upload.py 50000 ./client/client_file.txt
+2. Upload client for switching: python anon_client_upload.py 50000 ./client/client_file.txt:50002:50003
--- server.py
+++ server.py
@@ -11,6 +11,8 @@ REMOTE_SERVER_PORT = {}
 FTP_DIRECTORY_FILES = {}
 SERVER_DIRECTORY = ""
 
+STOR_COMPLETE_FILE_PATH = ""
+
 class AnonConnFTPHandler(FTPHandler):
 
     def on_connect(self):
@@ -38,34 +40,61 @@ class AnonConnFTPHandler(FTPHandler):
 
         if file_name in FTP_DIRECTORY_FILES:
             super().ftp_RETR(local_file_path)
-        else: 
+        elif len(remote_ftp_ports) > 0: # forwarding request is possible
             remote_ftp_port = int(remote_ftp_ports[0])
             remote_ftp_ports = ':'.join(remote_ftp_ports[1:])
             with open(local_file_path, "wb") as local_file_fp:
                 remote_file_name = file_name
                 if len(remote_ftp_ports) > 0:
                     remote_file_name = remote_file_name + ":" + remote_ftp_ports
-                self.remote_ftps[remote_ftp_port].retrbinary("RETR " + file_name + ":" + remote_ftp_ports, local_file_fp.write, 1024) # now local server has the file
+                self.remote_ftps[remote_ftp_port].retrbinary("RETR " + remote_file_name, local_file_fp.write, 1024) # now local server has the file
             super().ftp_RETR(local_file_path)
 
         for remote_ftp_port in REMOTE_SERVER_PORT.keys():
             self.remote_ftps[remote_ftp_port].quit()
 
     def ftp_STOR(self, complete_file_path):
+        # we do this because `complete_file_path` doesn't contain the forwarding ports
+        global STOR_COMPLETE_FILE_PATH
+        STOR_COMPLETE_FILE_PATH = complete_file_path 
+
         print("STOR requested for: ", complete_file_path)
         complete_file_path_content = complete_file_path.split("/")
-        file_name = complete_file_path_content[-2]
+        file_name_with_ports = complete_file_path_content[-1]
+
+        file_name_with_ports_content = file_name_with_ports.split(":")
+        file_name = file_name_with_ports_content[0]
+        remote_ftp_ports = file_name_with_ports_content[1:]
         local_file_path = SERVER_DIRECTORY + "/" + file_name
+
+        print("Local file path: ")
+        print(local_file_path)
+        print("Remote ftp ports: ")
+        print(remote_ftp_ports)
+
         super().ftp_STOR(local_file_path)
           
     def on_file_received(self, complete_file_path):
+        global STOR_COMPLETE_FILE_PATH
+        complete_file_path = STOR_COMPLETE_FILE_PATH
         print("on file received for: ", complete_file_path)
+
         complete_file_path_content = complete_file_path.split("/")
-        file_name = complete_file_path_content[-2]
-        remote_ftp_port = int(complete_file_path_content[-1])
+        file_name_with_ports = complete_file_path_content[-1]
+
+        file_name_with_ports_content = file_name_with_ports.split(":")
+        file_name = file_name_with_ports_content[0]
+        remote_ftp_ports = file_name_with_ports_content[1:]
         local_file_path = SERVER_DIRECTORY + "/" + file_name
-        with open(local_file_path, 'rb') as local_file_fp:
-            self.remote_ftps[remote_ftp_port].storbinary("STOR " + file_name, local_file_fp)
+
+        if len(remote_ftp_ports) > 0: # continue to send data
+            remote_ftp_port = int(remote_ftp_ports[0])
+            remote_ftp_ports = ':'.join(remote_ftp_ports[1:])
+            with open(local_file_path, "rb") as local_file_fp:
+                remote_file_name = file_name
+                if remote_ftp_ports != "":
+                    remote_file_name = remote_file_name + ":" + remote_ftp_ports
+                self.remote_ftps[remote_ftp_port].storbinary("STOR " + remote_file_name, local_file_fp)
         for remote_ftp_port in REMOTE_SERVER_PORT.keys():
             self.remote_ftps[remote_ftp_port].quit()
 
